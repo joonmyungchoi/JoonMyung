af69a41a27cfc80b1957b4998ec96aeebd044343
Choi JoonMyung <pizard@korea.ac.kr>
2023-02-10 12:53:56 +0900

    [1.3.0] Commit

--- playground/README.md
+++
@@ -1,4 +0,0 @@
-# Playground
-JoonMyung Choi's Package
-### wandb
-- wandb와 관련한 라이브러리 테스트
--- playground/profiling/test/randperm_topk.py
+++
@@ -1,7 +0,0 @@
-import torch
-def randperm(T, B, T_mix):
-    return torch.stack([torch.randperm(T, device="cpu") for i in range(B)])[:,:T_mix]
-
-def topK(T, B, T_mix):
-    return torch.topk(torch.randn(B,T, device="cuda"), T_mix, dim=1)[1]
-
